e7561ce2f0cb6ef504b1acb000b1aeca20d1e4c7
nakamura <hrm.nkmr04@gmail.com>
2025-04-25 16:03:55 +0900

    Appコンポーネントの型エラー対応。依存関係を整えてもbuildエラーが解消せず、相談したいです

--- packages/chapter-4/src/pages/_app.tsx
+++ packages/chapter-4/src/pages/_app.tsx
@@ -1,13 +1,7 @@
-import { AppProps } from 'next/app';
-import React from 'react';
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import React from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  // `Component` を `React.ComponentType` にキャスト
-  const WrappedComponent = Component as React.ComponentType<any>;
-
-  return <WrappedComponent {...pageProps} />;
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  return <Component {...pageProps} />;
 }
-
-
-// import loadsh from "lodash";
-// loadsh.filter([1, 2, 3], (item) => item > 1);
