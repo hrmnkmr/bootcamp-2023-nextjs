e725065adb656f3536e0da0a321102c7395abd84
nakamura <hrm.nkmr04@gmail.com>
2025-06-27 10:15:09 +0900

    fix: ビルド時に isPrismaError が見つからないエラー発生 (ts2305)

--- packages/chapter-6/src/components/TagList.tsx
+++ packages/chapter-6/src/components/TagList.tsx
@@ -1,7 +1,20 @@
-export const TagList = ({ tags }: { tags: { id: number; name: string }[] }) => (
-  <ul>
-    {tags.map((tag) => (
-      <li key={tag.id}>#{tag.name}</li>
-    ))}
-  </ul>
-);
+// src/components/TagList.tsx
+
+type Tag = {
+  id: number;
+  name: string;
+};
+
+type Props = {
+  tags: Tag[];
+};
+
+export default function TagList({ tags }: Props) {
+  return (
+    <ul>
+      {tags.map((tag) => (
+        <li key={tag.id}>{tag.name}</li>
+      ))}
+    </ul>
+  );
+}
--- packages/chapter-6/src/lib/next/apiRoutes.ts
+++ packages/chapter-6/src/lib/next/apiRoutes.ts
@@ -2,13 +2,13 @@ import { failed } from "@/services/client/apiRoutes";
 import type { Result } from "@/type";
 import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 import { ZodError } from "zod";
-import { isPrismaError } from "../prisma";
+import { isPrismaError } from "@/lib/prisma";
 
 export function apiHandler<T>(callback: NextApiHandler<Result<T>>) {
   return async (req: NextApiRequest, res: NextApiResponse<Result<T>>) => {
     try {
       return await callback(req, res);
-    } catch (err) {
+    } catch (err: unknown) {
       if (err instanceof ZodError) {
         res.status(400).json(failed({ message: "Invalid Request" }));
         return;
